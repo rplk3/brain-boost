b8858d57bc5b533ea0d99f075145f5847da5dbcf
Ranuda Perera <ranudaperera@gmail.com>
2026-02-12 23:47:44 +0530

    Activity 1 Lab02

---
+++ C/Lan03-Activity01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+    int mark1, mark2;
+    float average;
+
+    printf ("Input the first mark: ");
+    scanf ("%d", &mark1);
+
+    printf ("Input the second mark: ");
+
+}

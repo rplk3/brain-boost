952806a5240f7fa06992cfc89aa36c284a89e510
Ranuda Perera <ranudaperera@gmail.com>
2026-02-12 23:53:58 +0530

    Wrong data type

--- C/Lan03-Activity01.c
+++ C/Lan03-Activity01.c
@@ -7,5 +7,10 @@ int main (){
     scanf ("%d", &mark1);
 
     printf ("Input the second mark: ");
+    scanf ("%d", &mark2);
 
+    average = (mark1+mark2)/2.0;
+    printf ("The average is: %f\n", average);
+
+    return 0;
 }
